8030fc966bd0fb2925ee804aa6305e7d99a5f490
iamgadmarconi <gadmarconi4@outlook.com>
2025-04-10 22:33:38 +0100

    Remove `lissajous.py` file as part of codebase cleanup. This file previously contained functionality for Lissajous orbit calculations, which is no longer needed.

--- src/orbits/verticallyapunov.py
+++ src/orbits/verticallyapunov.py
@@ -9,3 +9,6 @@ from system.libration import CollinearPoint, L1Point, L2Point, L3Point
 from algorithms.geometry import _gamma_L, _find_y_zero_crossing
 from algorithms.dynamics import compute_stm
 from log_config import logger
+
+
+class VerticalLyapunovOrbit(PeriodicOrbit):
