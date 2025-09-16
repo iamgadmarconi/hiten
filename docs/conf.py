2423ed66ff169766c1d1341c5dcbfd973efddea8
iamgadmarconi <gadmarconi4@outlook.com>
2025-09-16 17:59:23 +0100

    Remove display version option from Sphinx configuration in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,6 @@ html_theme_options = {
     "style_external_links": True,
     "style_nav_header_background": "#1a1a1a",
     "logo_only": True,
-    "display_version": False,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
